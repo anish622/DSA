d83231447193a253680e99db9dd04d687bf1e643
anish622 <anishanigowda73@gmail.com>
2025-07-19 10:38:39 +0530

    through commit
    concatinating the two list

---
+++ can.py
@@ -0,0 +1,15 @@
+list1=[]
+for i in range(4):
+    num = int(input("enter a numbers"))
+    list1.append(num)
+print(list1)
+
+list2 = []
+for i in range(4):
+    a = int(input("enter a number"))
+    list2.append(a)
+print(list2)
+
+result = list1+list2
+print(result)
+
