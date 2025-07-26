af9042dc74927be048c919789111aa00e0b296cc
anish622 <anishanigowda73@gmail.com>
2025-07-26 09:48:51 +0530

    commit

---
+++ dict.py
@@ -0,0 +1,8 @@
+n = int(input("enter a number of items"))
+my_dict = {}
+for i in range(n):
+    key = input("enter a keys")
+    values = input("enter a values")
+    my_dict[key] = values
+print("Dictionary",my_dict)
+
